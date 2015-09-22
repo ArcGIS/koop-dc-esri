0aa7e7cca84e721b9cce0bcb5cc7e3c1c1e20ee1
dmfenton <dfenton@esri.com>
2015-09-22 15:10:42 -0400

    use cluster mode

--- server.js
+++ server.js
@@ -17,53 +17,70 @@ var express = require("express"),
   pgCache = require('koop-pgcache'),
   tiles = require('koop-tile-plugin');
 
-// this is not required but is helpful
-koop.registerCache( pgCache );
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length
 
-//register providers with koop
-koop.register( socrata );
-koop.register( ckan );
-koop.register( github );
-koop.register( gist );
-koop.register( agol );
-koop.register( acs );
-koop.register( osm );
-koop.register( opendata );
-koop.register( fda );
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-// register the tiles plugin
-koop.register( tiles );
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  // this is not required but is helpful
+  koop.registerCache( pgCache );
 
-// create an express app
-var app = express();
-app.use( cors() );
+  //register providers with koop
+  koop.register( socrata );
+  koop.register( ckan );
+  koop.register( github );
+  koop.register( gist );
+  koop.register( agol );
+  koop.register( acs );
+  koop.register( osm );
+  koop.register( opendata );
+  koop.register( fda );
 
-app.use(function(req,res,next){
-  var oldEnd = res.end;
+  // register the tiles plugin
+  koop.register( tiles );
 
-  res.end = function() {
-    oldEnd.apply(res, arguments);
-  };
+  // create an express app
+  var app = express();
+  app.use( cors() );
 
-  next();
-});
+  app.use(function(req,res,next){
+    var oldEnd = res.end;
 
-app.use(function (req, res, next) {
-  res.removeHeader("Vary");
-  next();
-});
+    res.end = function() {
+      oldEnd.apply(res, arguments);
+    };
 
-// add koop middleware
-app.use( koop );
+    next();
+  });
 
-app.get('/status', function(req, res){
-  res.json( koop.status );
-});
+  app.use(function (req, res, next) {
+    res.removeHeader("Vary");
+    next();
+  });
 
-app.set('view engine', 'ejs');
-app.use(express.static('views/public'));
+  // add koop middleware
+  app.use( koop );
 
-app.listen(process.env.PORT || config.server.port,  function() {
-  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
-});
+  app.get('/status', function(req, res){
+    res.json( koop.status );
+  });
+
+  app.set('view engine', 'ejs');
+  app.use(express.static('views/public'));
+
+  app.listen(process.env.PORT || config.server.port,  function() {
+    console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+  });
+}
+
+
 
